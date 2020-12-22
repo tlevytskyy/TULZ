4272c0a211384e654b5527aa73e7211ab5afc65d
Arren Ting <ating1@nait.ca>
2020-12-22 14:12:07 -0700

    Created Min,Max,Close button. Redesigned layout of the program.

--- TULZ/Form1.Designer.cs
+++ TULZ/Form1.Designer.cs
@@ -29,81 +29,46 @@ namespace TULZ
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panelTimerSubMenu = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnTimer1 = new System.Windows.Forms.Button();
             this.btnTimer = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panelTimerSubMenu = new System.Windows.Forms.Panel();
-            this.btnTimer1 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelTopPanel = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panelTimerSubMenu.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panelTopPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button2
+            // btnMinimize
             // 
-            this.button2.Location = new System.Drawing.Point(837, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(27, 24);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(889, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(27, 24);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(784, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(27, 24);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnMinimize.Location = new System.Drawing.Point(819, 12);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(27, 24);
+            this.btnMinimize.TabIndex = 4;
+            this.btnMinimize.Text = "button3";
+            this.btnMinimize.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(53)))), ((int)(((byte)(55)))));
             this.panel1.Controls.Add(this.panelTimerSubMenu);
             this.panel1.Controls.Add(this.btnTimer);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(218, 544);
+            this.panel1.Size = new System.Drawing.Size(127, 544);
             this.panel1.TabIndex = 6;
-            // 
-            // btnTimer
-            // 
-            this.btnTimer.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnTimer.FlatAppearance.BorderSize = 0;
-            this.btnTimer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTimer.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnTimer.Location = new System.Drawing.Point(0, 150);
-            this.btnTimer.Name = "btnTimer";
-            this.btnTimer.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnTimer.Size = new System.Drawing.Size(218, 34);
-            this.btnTimer.TabIndex = 1;
-            this.btnTimer.Text = "Timer";
-            this.btnTimer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnTimer.UseVisualStyleBackColor = true;
-            // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(218, 150);
-            this.panel2.TabIndex = 0;
             // 
             // panelTimerSubMenu
             // 
@@ -113,86 +78,183 @@ namespace TULZ
             this.panelTimerSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTimerSubMenu.Location = new System.Drawing.Point(0, 184);
             this.panelTimerSubMenu.Name = "panelTimerSubMenu";
-            this.panelTimerSubMenu.Size = new System.Drawing.Size(218, 109);
+            this.panelTimerSubMenu.Size = new System.Drawing.Size(127, 106);
             this.panelTimerSubMenu.TabIndex = 2;
-            // 
-            // btnTimer1
-            // 
-            this.btnTimer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
-            this.btnTimer1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnTimer1.FlatAppearance.BorderSize = 0;
-            this.btnTimer1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTimer1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnTimer1.Location = new System.Drawing.Point(0, 0);
-            this.btnTimer1.Name = "btnTimer1";
-            this.btnTimer1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnTimer1.Size = new System.Drawing.Size(218, 34);
-            this.btnTimer1.TabIndex = 0;
-            this.btnTimer1.Text = "Timer1";
-            this.btnTimer1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnTimer1.UseVisualStyleBackColor = false;
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
-            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button5.Location = new System.Drawing.Point(0, 34);
-            this.button5.Name = "button5";
-            this.button5.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.button5.Size = new System.Drawing.Size(218, 34);
-            this.button5.TabIndex = 1;
-            this.button5.Text = "Timer2";
-            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button5.UseVisualStyleBackColor = false;
             // 
             // button6
             // 
-            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(125)))), ((int)(((byte)(58)))));
             this.button6.Dock = System.Windows.Forms.DockStyle.Top;
             this.button6.FlatAppearance.BorderSize = 0;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button6.ForeColor = System.Drawing.Color.Black;
             this.button6.Location = new System.Drawing.Point(0, 68);
             this.button6.Name = "button6";
             this.button6.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.button6.Size = new System.Drawing.Size(218, 34);
+            this.button6.Size = new System.Drawing.Size(127, 34);
             this.button6.TabIndex = 2;
             this.button6.Text = "Timer3";
             this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button6.UseVisualStyleBackColor = false;
             // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(125)))), ((int)(((byte)(58)))));
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(0, 34);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(127, 34);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Timer2";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // btnTimer1
+            // 
+            this.btnTimer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(125)))), ((int)(((byte)(58)))));
+            this.btnTimer1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTimer1.FlatAppearance.BorderSize = 0;
+            this.btnTimer1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTimer1.ForeColor = System.Drawing.Color.Black;
+            this.btnTimer1.Location = new System.Drawing.Point(0, 0);
+            this.btnTimer1.Name = "btnTimer1";
+            this.btnTimer1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnTimer1.Size = new System.Drawing.Size(127, 34);
+            this.btnTimer1.TabIndex = 0;
+            this.btnTimer1.Text = "Timer1";
+            this.btnTimer1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTimer1.UseVisualStyleBackColor = false;
+            // 
+            // btnTimer
+            // 
+            this.btnTimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(125)))), ((int)(((byte)(58)))));
+            this.btnTimer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTimer.FlatAppearance.BorderSize = 0;
+            this.btnTimer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTimer.ForeColor = System.Drawing.Color.Black;
+            this.btnTimer.Location = new System.Drawing.Point(0, 150);
+            this.btnTimer.Name = "btnTimer";
+            this.btnTimer.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnTimer.Size = new System.Drawing.Size(127, 34);
+            this.btnTimer.TabIndex = 1;
+            this.btnTimer.Text = "Timer";
+            this.btnTimer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTimer.UseVisualStyleBackColor = false;
+            this.btnTimer.Click += new System.EventHandler(this.btnTimer_Click);
+            this.btnTimer.MouseEnter += new System.EventHandler(this.btnTimer_MouseEnter);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(53)))), ((int)(((byte)(55)))));
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(127, 150);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(53)))), ((int)(((byte)(55)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(127, 51);
+            this.panel3.TabIndex = 0;
+            // 
+            // panelTopPanel
+            // 
+            this.panelTopPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelTopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(53)))), ((int)(((byte)(55)))));
+            this.panelTopPanel.Controls.Add(this.btnClose);
+            this.panelTopPanel.Controls.Add(this.btnMin);
+            this.panelTopPanel.Controls.Add(this.btnMax);
+            this.panelTopPanel.Location = new System.Drawing.Point(0, 0);
+            this.panelTopPanel.Name = "panelTopPanel";
+            this.panelTopPanel.Size = new System.Drawing.Size(918, 51);
+            this.panelTopPanel.TabIndex = 7;
+            this.panelTopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseDown);
+            this.panelTopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseMove);
+            this.panelTopPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseUp);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(878, 11);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(36, 34);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "x";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMin.Location = new System.Drawing.Point(768, 12);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(36, 34);
+            this.btnMin.TabIndex = 10;
+            this.btnMin.Text = "-";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnMax
+            // 
+            this.btnMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMax.Location = new System.Drawing.Point(826, 12);
+            this.btnMax.Name = "btnMax";
+            this.btnMax.Size = new System.Drawing.Size(36, 32);
+            this.btnMax.TabIndex = 8;
+            this.btnMax.Text = "oo";
+            this.btnMax.UseVisualStyleBackColor = true;
+            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            // 
             // panelSideMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 544);
+            this.ClientSize = new System.Drawing.Size(915, 544);
+            this.Controls.Add(this.panelTopPanel);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "panelSideMenu";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panelTimerSubMenu.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panelTopPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnMinimize;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnTimer;
         private System.Windows.Forms.Panel panelTimerSubMenu;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button btnTimer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelTopPanel;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMin;
     }
 }
 
--- TULZ/Form1.cs
+++ TULZ/Form1.cs
@@ -11,15 +11,87 @@ using System.Windows.Forms;
 namespace TULZ
 {
     public partial class panelSideMenu : Form
-    {
+    {   
+        int mov;
+        int movX;
+        int movY;
+        
+
         public panelSideMenu()
         {
             InitializeComponent();
+            InitializeMenus();
         }
+
+        private void InitializeMenus()
+        {
+            panelTimerSubMenu.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelTimerSubMenu.Visible == true)
+                panelTimerSubMenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel submenu)
+        {
+            if (submenu.Visible == false)
+            {
+                hideSubMenu();
+                submenu.Visible = true;
+            }
+            else
+                submenu.Visible = false;
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void btnTimer_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelTimerSubMenu);
+        }
+
+        private void btnTimer_MouseEnter(object sender, EventArgs e)
+        {
+        }
+        //----Move Form Args----------------------------------------------------------------------------------
+        private void panel4_MouseUp(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+        private void panel4_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+        private void panel4_MouseMove(object sender, MouseEventArgs e)
+        {
+            if  (mov == 1) this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+        }
+        //----TopBarButtons----------------------------------------------------------------------------------
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //----------------------------------------------------------------------------------------------------
     }
 }
